f567c20d3961ad9fc16cd3332525ceb429cd4337
DavidMarvel2409 <david.marvel.programer.2409@gmail.com>
2023-12-12 21:32:45 -0300

    update 12/12/2023 a las 21:32
    weas del acerca de

--- Prototipo/Assets/scripts/DialogosControler.cs
+++ Prototipo/Assets/scripts/DialogosControler.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DialogosControler : MonoBehaviour
 {
@@ -11,12 +12,16 @@ public class DialogosControler : MonoBehaviour
     private int index_text = 0;
     public GameObject pantalla_dialogo;
     public GameObject cara2;
+    public GameObject[] Pantallas_fuera_del_jefe;
+    public GameObject[] Imagenes_de_fondo;
 
     public GameObject nave;
+    private string nivel;
 
     void Start()
     {
         texto.text = Dialogo[index_text];
+        nivel = SceneManager.GetActiveScene().name;
     }
 
 
@@ -38,8 +43,26 @@ public class DialogosControler : MonoBehaviour
         }
         if (index_text ==  Dialogo.Length - 1)
         {
-            cara2.SetActive(true);
-            nave.SetActive(true);
+            if (nivel == "Nivel 3")
+            {
+                cara2.SetActive(true);
+                nave.SetActive(true);
+            }
+            else if (nivel == "acercade")
+            {
+                if (Imagenes_de_fondo[0].name == "noticias")
+                {
+                    Imagenes_de_fondo[0].SetActive(false);
+                    Imagenes_de_fondo[1].SetActive(true);
+                    Pantallas_fuera_del_jefe[1].SetActive(true);
+                    Pantallas_fuera_del_jefe[0].SetActive(false);
+                }
+                else if (Imagenes_de_fondo[0].name == "noticiados")
+                {
+                    //aqui falta colocar la wea para cuando se terminan los dialogos del cientifico (ya me quiero ir a la shit)
+                }
+            }
+
         }
     }
 }
